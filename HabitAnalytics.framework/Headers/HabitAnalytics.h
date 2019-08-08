7cc1dd1761cbc8180002a13658742f10e6e0839d
Ana Figueira <ana.figueira@habit.io>
2019-08-08 13:44:11 +0100

    Adds version 1.5.0 of framework

---
+++ HabitAnalytics.framework/Headers/HabitAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  HabitAnalytics.h
+//  HabitAnalytics
+//
+//  Created by Ana Figueira on 26/06/2019.
+//  Copyright © 2019 Habit Analytics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HabitAnalytics.
+FOUNDATION_EXPORT double HabitAnalyticsVersionNumber;
+
+//! Project version string for HabitAnalytics.
+FOUNDATION_EXPORT const unsigned char HabitAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HabitAnalytics/PublicHeader.h>
+
+
